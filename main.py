59c85d368795376c203e69be60dd3ab19dd7e02a
justhuman1008 <seanking1008@naver.com>
2022-11-06 18:48:19 +0900

    정보 저장방식 변경

--- main.py
+++ main.py
@@ -2,27 +2,18 @@ import discord
 from discord.commands import Option
 from discord.ui import View, InputText, Modal
 
-from hcskr import asyncSelfCheck
+import asyncio
+
+import requests
 import json
 
 from os import listdir
 from sys import exit
 
-try:
-    from setting import token, owner, guild, hcs_path, ImageDict
-    BOT_token = token
-    file_path = hcs_path
-except:
-    print("=========================")
-    print("[ setting.py ] 에서 정보를 불러오지 못했습니다.")
-    print()
-    print("환경변수가 제대로 작성되어있는지 확인해주세요")
-    print("=========================") 
-    exit()
-
+from setting import token, Neis_key, owner, guild, DB_path, ImageDict, Areas
+from hcskr import asyncSelfCheck
 
 bot = discord.Bot()
-
 
 @bot.event # 봇 작동
 async def on_ready():
@@ -39,7 +30,6 @@ for filename in listdir('./cogs'): # Cogs 자동 로드(봇 작동시)
         print(f'{filename[:-3]}가 정상적으로 로드되었습니다.')
 
 
-
 @bot.slash_command(description="봇 도움말 확인")
 async def 도움말(ctx):
     Help_Embed = discord.Embed(title=f"{bot.user.name} 도움말", description=f"­", colour=0xffdc16)
@@ -49,7 +39,7 @@ async def 도움말(ctx):
     Help_Embed.add_field(name=f"자가진단 실행하기", value=f":small_blue_diamond: /자가진단 실행", inline=True)
     Help_Embed.add_field(name=f"자가진단 예약하기", value=f":small_blue_diamond: /자가진단 예약", inline=True)
     Help_Embed.add_field(name="­", value=f"­", inline=True)
-    Help_Embed.add_field(name="­", value=f"­", inline=False)
+    Help_Embed.add_field(name="급식 조회하기", value=f":small_blue_diamond: /급식", inline=False)
     Help_Embed.add_field(name=f"명령어 목록 확인하기", value=f":small_blue_diamond: /도움말", inline=True)
     Help_Embed.add_field(name=f"봇의 정보 확인하기", value=f":small_blue_diamond: /정보", inline=True)
     Help_Embed.add_field(name="­", value=f"­", inline=True)
@@ -81,7 +71,7 @@ async def ping(ctx):
 @bot.command(description=f"봇에 대한 정보를 출력합니다.")
 async def 정보(ctx):
     try:
-        users = json.load(open(hcs_path,encoding="utf_8"))['users']
+        users = json.load(open(DB_path,encoding="utf_8"))['users']
     except:
         users = "Erorr"
 
@@ -91,8 +81,8 @@ async def 정보(ctx):
     bot_info.add_field(name="­", value="­", inline=True)
     bot_info.add_field(name="연결된 서버 수⠀⠀⠀", value=f'`{len(bot.guilds)}개 서버`', inline=True)
     bot_info.add_field(name="이용중인 유저 수", value=f'`{users}명`', inline=True)
-    bot_info.add_field(name="­", value="­", inline=True)
-    bot_info.add_field(name="개발 언어", value="Python [Pycord](https://docs.pycord.dev/en/master/)", inline=True)
+    bot_info.add_field(name="지원 서버", value="[서포트 서버](https://discord.gg/PS4UttDmPQ)", inline=True)
+    bot_info.add_field(name="라이브러리", value="[Pycord](https://docs.pycord.dev/en/master/)", inline=True)
     bot_info.add_field(name='GitHub', value='[Bot GitHub](https://github.com/justhuman1008/JustBot)', inline=True)
     bot_info.add_field(name="호스팅", value="Galaxy S9 UserLAnd", inline=True)
     bot_info.add_field(name="소유자", value=f"{owner}", inline=False)
@@ -114,16 +104,19 @@ async def invite(ctx):
 
 @bot.slash_command(guild_ids=[guild], description="봇에서 유저 DB를 다운받습니다.")
 async def db다운(ctx):
-    await ctx.respond(file=discord.File(hcs_path))
+    await ctx.respond(file=discord.File(DB_path))
     print("관리자가 유저 DB를 다운로드 받았습니다.")
 
-#=================================================================================================================================
-#=================================================================================================================================
-#=================================================================================================================================
-
-
+#====================================================================================================================================================
+#====================================================================================================================================================
+#====================================================================================================================================================
+# 함수 모음
 def is_register(DiscordID):
-    with open(file_path, "r", encoding="utf_8") as json_file:
+    """
+    유저가 자신의 정보를 입력했는지 확인하는 함수\n
+    가입되어있다면 True, 아니라면 False를 반환
+    """
+    with open(DB_path, "r", encoding="utf_8") as json_file:
         contents = json_file.read()
         json_data = json.loads(contents)
     try:
@@ -134,12 +127,27 @@ def is_register(DiscordID):
 
 
 def add_info(Nickname, DiscordID, Name, Birthday, Area, School, School_lv, Password):
-    register = is_register(DiscordID)
+    """
+    DB에 유저의 정보를 입력하는 함수\n
+    입력하는 순서는 닉네임, 아이디, 본명, 생년월일(6자), 지역명, 학교명, 학교분류, 비밀번호
+    """
+    register = is_register(DiscordID) # 가입되어 있을시 중지
     if register:
         return
 
+    API_Main = "https://open.neis.go.kr/hub/schoolInfo?"
+    API_String = f"KEY={Neis_key}&Type=json&pIndex=1&pSize=5&SCHUL_NM={School}&SCHUL_KND_SC_NM={School_lv}"
+    API = API_Main + API_String
+
+    SchoolAPI = requests.get(API).json()
+    School_code = SchoolAPI['schoolInfo'][1]['row'][0]['SD_SCHUL_CODE']
+    Moe_code = SchoolAPI['schoolInfo'][1]['row'][0]['ATPT_OFCDC_SC_CODE']
+
+
+    Area = Areas[Moe_code][1]
+
     data = {}
-    with open(file_path, "r", encoding="utf_8") as json_file:
+    with open(DB_path, "r", encoding="utf_8") as json_file:
         data = json.load(json_file)
 
     data[f'{DiscordID}'] = []
@@ -151,29 +159,38 @@ def add_info(Nickname, DiscordID, Name, Birthday, Area, School, School_lv, Passw
         "Area": f"{Area}",
         "School": f"{School}",
         "School_lv": f"{School_lv}",
+        "School_code": f"{School_code}",
+        "Moe_code": f"{Moe_code}",
         "Password": f"{Password}",
         "Auto_check": f"O"})
 
-    with open(file_path, 'w',encoding="utf_8") as writefile:
+    with open(DB_path, 'w',encoding="utf_8") as writefile:
         json.dump(data, writefile, indent="\t", ensure_ascii=False)
 
 
 def delete_info(DiscordID):
+    """
+    DB에 유저의 정보를 삭제하는 함수
+    """
     register = is_register(DiscordID)
     if register:
-        with open(file_path, "r", encoding="utf_8") as json_file:
+        with open(DB_path, "r", encoding="utf_8") as json_file:
             contents = json_file.read()
             json_data = json.loads(contents)
 
         json_data.pop(f'{DiscordID}')
         
-        with open(file_path, 'w',encoding="utf_8") as writefile:
+        with open(DB_path, 'w',encoding="utf_8") as writefile:
             json.dump(json_data, writefile, indent="\t", ensure_ascii=False)
 
         return True
     else:
         return False
 
+
+#====================================================================================================================================================
+#====================================================================================================================================================
+#====================================================================================================================================================
 
 @bot.slash_command(description="교육부 자가진단을 진행합니다.")
 async def 자가진단(ctx, 작업:Option(str,"다음 중 하나를 선택하세요.", choices=["실행", "예약", "등록", "삭제"])):
@@ -183,11 +200,11 @@ async def 자가진단(ctx, 작업:Option(str,"다음 중 하나를 선택하세
     UserID = str(ctx.author.id)
 
     not_registered = discord.Embed(title="등록되지 않은 사용자입니다.", description="해당 기능을 이용하시려면 `/자가진단 등록`을 입력해주세요", color=0xffdc16)
-    not_registered.set_thumbnail(url=ImageDict["List"])
-
+    not_registered.set_thumbnail(url=ImageDict["List_Failed"])
+    
     if 작업 == "실행":
         if TrueFalse:
-            with open(file_path, "r", encoding="utf_8") as json_file:
+            with open(DB_path, "r", encoding="utf_8") as json_file:
                 json_data = json.load(json_file)
             Name = json_data[UserID][0]['Name']
             Birthday = json_data[UserID][0]['Birthday']
@@ -216,7 +233,7 @@ async def 자가진단(ctx, 작업:Option(str,"다음 중 하나를 선택하세
 
     if 작업 == "삭제":
         Delete = discord.Embed(title=f"{bot.user.name} 자가진단", description="­", color=0xffdc16)
-        Delete.add_field(name="진단정보 제거시 `/자가진단 진행`을 사용할 수 없습니다.", value="자가진단 정보를 제거하시려면 `1분`내로 ✅를 클릭해주세요.", inline=False)
+        Delete.add_field(name="진단정보 제거시 `자가진단 매크로, 급식조회가 불가능`합니다.", value="자가진단 정보를 제거하시려면 `1분`내로 ✅를 클릭해주세요.", inline=False)
         Delete.set_thumbnail(url=ImageDict["Trash_can"])
 
         Delete_Failed = discord.Embed(title="진단정보 삭제가 취소되었습니다", description="­자가진단 정보를 삭제하시려면 다시 `/자가진단 삭제`를 입력해주세요.", color=0xffdc16)
@@ -231,15 +248,14 @@ async def 자가진단(ctx, 작업:Option(str,"다음 중 하나를 선택하세
                         Delete_Success = discord.Embed(title=f"{ctx.author.name}님의 진단정보 삭제가 완료되었습니다.", description="추후 자가진단을 진행하시려면 다시 `/자가진단 등록`을 입력해주세요", color=0xffdc16)
                         Delete_Success.set_thumbnail(url=ImageDict["Trash_can"])
                         await Question.edit_original_message(embed=Delete_Success, view=None)
-                        self.timeout_disable = True
 
                 @discord.ui.button(style=discord.ButtonStyle.red, emoji="⛔")
                 async def Nope(self, button: discord.ui.Button, interaction: discord.Interaction):
                     await Question.edit_original_message(embed=Delete_Failed, view=None)
+                    
 
                 async def on_timeout(self):
-                    if not self.timeout_disable == True:
-                        await Question.edit_original_message(embed=Delete_Failed, view=None)
+                        await Question.edit_original_message(view=None)
 
             Question = await ctx.respond(embed=Delete, view=Button(timeout=60))
         else:
@@ -247,16 +263,9 @@ async def 자가진단(ctx, 작업:Option(str,"다음 중 하나를 선택하세
 
 
     if 작업 == "등록":
-        register = discord.Embed(title=f"{bot.user.name} 자가진단", description="­자가진단 정보 입력시  `자가진단용 정보가 저장`되어\n`/자가진단 진행`을 사용할 수 있습니다.", color=0xffdc16)
-        register.add_field(name="­", value="❗ 자가진단 정보를 확인하기 위해 `1회 자가진단이 진행됩니다.`\n\n자가진단 정보를 입력하시려면 `1분`내로 ✅를 클릭해주세요.", inline=False)
-        register.set_thumbnail(url=ImageDict["List"])
-
-        Register_Failed = discord.Embed(title="자가진단 등록이 취소되었습니다.", description="­자가진단 정보를 입력하시려면 다시 `/자가진단 등록`을 입력해주세요", color=0xffdc16)
-        Register_Failed.set_thumbnail(url=ImageDict["List"])
-
         if TrueFalse:
             Register_Already = discord.Embed(title="이미 정보가 입력되어 있습니다.", description="­자가진단 정보를 삭제하시려면 `/자가진단 삭제`를 입력해주세요", color=0xffdc16)
-            Register_Already.set_thumbnail(url=ImageDict["List"])
+            Register_Already.set_thumbnail(url=ImageDict["List_Done"])
             await ctx.respond(embed=Register_Already)
         else:
             class infoQ(Modal):
@@ -284,7 +293,19 @@ async def 자가진단(ctx, 작업:Option(str,"다음 중 하나를 선택하세
                     School = self.children[3].value
                     Password = self.children[4].value
                     
-                    #print(Name,Birthday,Area,School,Password,School_lv)
+
+                    Register_Failed = discord.Embed(title="자가진단 등록이 취소되었습니다.", description="­자가진단 정보를 입력하시려면 다시 `/자가진단 등록`을 입력해주세요", color=0xffdc16)
+                    Register_Failed.set_thumbnail(url=ImageDict["List_Failed"])
+
+                    reg_ntc1 = "❗ 자가진단 정보를 확인하기 위해 `1회 자가진단이 진행됩니다.`\n\n"
+                    reg_ntc2 = "이후 `제공하신 정보가 저장`되며\n 유저가 `직접 삭제하기 전까지` 서비스 제공을 위해 사용되며\n\n"
+                    reg_ntc3 = "`제3자에게 제공되지 않습니다.`\n\n 보유 목적: `서비스에 대한 공지`, `자가진단 서비스 진행`\n\n"
+                    reg_ntc4 = "자가진단 정보를 입력하시려면 `1분`내로 ✅를 클릭해주세요."
+                    reg_ntc = reg_ntc1+reg_ntc2+reg_ntc3+reg_ntc4
+
+                    register = discord.Embed(title=f"{bot.user.name} 자가진단", description=reg_ntc, color=0xffdc16)
+                    register.set_thumbnail(url=ImageDict["List_Write"])
+                    
                     class Button(discord.ui.View):
                         @discord.ui.button(style=discord.ButtonStyle.green, emoji="✅")
                         async def OK(self, button: discord.ui.Button, interaction: discord.Interaction):
@@ -293,56 +314,57 @@ async def 자가진단(ctx, 작업:Option(str,"다음 중 하나를 선택하세
                                 if hcskr_result['code'] == 'SUCCESS':
                                     add_info(ctx.author.name, ctx.author.id, Name, Birthday, Area, School, School_lv, Password)
 
-                                    Register_Success = discord.Embed(title=f"{ctx.author}님의 자가진단 정보 입력이 완료되었습니다.", description="`/자가진단 진행`으로 자가진단을 진행할 수 있습니다.",color=0xffdc16)
-                                    Register_Success.set_thumbnail(url=ImageDict["List"])
+                                    Register_Success = discord.Embed(title=f"{ctx.author}님의 자가진단 정보 입력이 완료되었습니다.", description="매일 오전 7시~7시 10분 사이에 자가진단이 진행됩니다.",color=0xffdc16)
+                                    Register_Success.set_thumbnail(url=ImageDict["List_Done"])
                                     print(f"{ctx.author}님의 자가진단 정보가 입력되었습니다.")
                                     await Question.edit_original_message(embed=Register_Success, view=None)
+
                                 else:
                                     error_reason = errorlist[hcskr_result['code']]
                                     Register_Test_Fail = discord.Embed(title=f"{ctx.author}님의 자가진단 정보 입력이 실패하였습니다.", description=f"입력된 오류: {error_reason}",color=0xffdc16)
-                                    Register_Test_Fail.set_thumbnail(url=ImageDict["List"])
+                                    Register_Test_Fail.set_thumbnail(url=ImageDict["List_Failed"])
                                     await Question.edit_original_message(embed=Register_Test_Fail, view=None)
+
                             except:
                                 error_reason = errorlist[hcskr_result['code']]
                                 Failed_reg = discord.Embed(title="자가진단 정보 등록에 실패했습니다.", description=f'정보를 모두 "정확히" 입력했는지 확인해주세요\n 입력된 오류: {error_reason}', color=0xffdc16)
-                                Failed_reg.set_thumbnail(url=ImageDict["List"])
+                                Failed_reg.set_thumbnail(url=ImageDict["List_Failed"])
                                 await Question.edit_original_message(embed=Failed_reg)
                                 
-                            self.timeout_disable = True
+                            
                             
                         @discord.ui.button(style=discord.ButtonStyle.red, emoji="⛔")
                         async def Nope(self, button: discord.ui.Button, interaction: discord.Interaction):
                             await Question.edit_original_message(embed=Register_Failed, view=None)
-                            self.timeout_disable = True
+                            
 
                         async def on_timeout(self):
-                            if not self.timeout_disable == True:
-                                await Question.edit_original_message(embed=Register_Failed, view=None)
+                            await Question.edit_original_message(view=None)
 
                     Question = await interaction.response.send_message(embed=register,view=Button(timeout=60))
             await ctx.interaction.response.send_modal(infoQ())
 
     if 작업 == "예약":
         if TrueFalse:
-            with open(file_path, "r", encoding="utf_8") as json_file:
+            with open(DB_path, "r", encoding="utf_8") as json_file:
                 json_data = json.load(json_file)
 
             if json_data[UserID][0]['Auto_check'] == "O":
                 json_data[UserID][0]['Auto_check'] = "X"
 
                 Auto_off = discord.Embed(title=f"{bot.user.name} 자가진단 매크로", description="­이제부터 자동 자가진단이 종료됩니다.", color=0xffdc16)
-                Auto_off.set_thumbnail(url=ImageDict["hcs_icon"])
+                Auto_off.set_thumbnail(url=ImageDict["Timer_Off"])
                 await ctx.respond(embed=Auto_off)
 
             else:
                 json_data[UserID][0]['Auto_check'] = "O"
 
                 Auto_on = discord.Embed(title=f"{bot.user.name} 자가진단 매크로", description="­이제부터 자가진단이 매일 오전 7시 ~ 7시 10분 사이에 진행됩니다.", color=0xffdc16)
-                Auto_on.set_thumbnail(url=ImageDict["hcs_icon"])
+                Auto_on.set_thumbnail(url=ImageDict["Timer_On"])
                 await ctx.respond(embed=Auto_on)
 
 
-            with open(file_path, 'w',encoding="utf_8") as writefile:
+            with open(DB_path, 'w',encoding="utf_8") as writefile:
                 json.dump(json_data, writefile, indent="\t", ensure_ascii=False)
         else:
             await ctx.respond(embed=not_registered)
@@ -362,19 +384,14 @@ async def 강제추가(ctx, 디스코드닉네임, 디스코드id, 이름, 생�
 
         add_info(디스코드닉네임, 디스코드id, 이름, 생년월일, 지역, 학교명, School_lv, 비밀번호)
 
-        with open(file_path, "r", encoding="utf_8") as json_file:
+        with open(DB_path, "r", encoding="utf_8") as json_file:
             json_data = json.load(json_file)
-            
-        if 예약유무 == "O":
-            json_data[디스코드id][0]['Auto_check'] = "O"
-        else:
-            json_data[디스코드id][0]['Auto_check'] = "X"
 
-        with open(file_path, 'w',encoding="utf_8") as writefile:
+        with open(DB_path, 'w',encoding="utf_8") as writefile:
             json.dump(json_data, writefile, indent="\t", ensure_ascii=False)
         await ctx.respond(f"`{디스코드닉네임}`님의 정보를 DB에 입력했습니다.")
         print(f"{디스코드닉네임}님의 자가진단 정보가 관리자 권한으로 입력되었습니다.")
     except:
         await ctx.respond("입력 실패")
 
-bot.run(BOT_token)
+bot.run(token)
